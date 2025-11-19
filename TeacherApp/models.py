b1cd6044e76c03389af3e4b186045fdcb3e77b8e
AleenaJoseph7 <aleenajoseph18122002@gmail.com>
2025-11-19 19:21:04 +0530

    commit 9 (Attendancedb added)

--- TeacherApp/models.py
+++ TeacherApp/models.py
@@ -11,9 +11,26 @@ class Studentdb(models.Model):
     Student_phone = models.IntegerField(null=True, blank=True)
     Student_prof=models.ImageField(upload_to="Student Profile Image",null=True,blank=True)
 
+    def __str__(self):
+        return self.Student_name
+
 class Subjectdb(models.Model):
     Subject_name=models.CharField(max_length=30,null=True,blank=True)
     Subject_code=models.CharField(max_length=30,null=True,blank=True)
     Subject_teacher=models.CharField(max_length=30,null=True,blank=True)
     Subject_sem=models.CharField(max_length=30,null=True,blank=True)
     Subject_dep=models.CharField(max_length=30,null=True,blank=True)
+
+    def __str__(self):
+        return self.Subject_code
+
+
+class Attendancedb(models.Model):
+    Student=models.ForeignKey(Studentdb,on_delete=models.CASCADE)
+    Subject=models.ForeignKey(Subjectdb,on_delete=models.CASCADE)
+    Date=models.DateField(auto_now_add=True)
+    Status=models.CharField(max_length=10,default="Absent",choices=[("Present","Present"),
+                                                                    ("Absent","Absent")])
+    # ("Present", "Present") 1st data to be stored to db, 2nd visible to user
+    def __str__(self):
+        return self.Student
