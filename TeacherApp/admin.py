b398633be589b81074a36fb814feabd7826fc3d1
AleenaJoseph7 <aleenajoseph18122002@gmail.com>
2025-11-26 20:01:08 +0530

    commit 32 (chatbotdb registered to admin))

--- TeacherApp/admin.py
+++ TeacherApp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from TeacherApp.models import Studentdb,Subjectdb,Attendancedb,Internalmarkdb
+from TeacherApp.models import Studentdb,Subjectdb,Attendancedb,Internalmarkdb,ChatMessage
 
 # Register your models here.
 admin.site.register(Subjectdb)
 admin.site.register(Studentdb)
 admin.site.register(Internalmarkdb)
 admin.site.register(Attendancedb)
+admin.site.register(ChatMessage)
