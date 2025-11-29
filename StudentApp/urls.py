668db8f4f910692d5018f9a1dad051e381967386
AleenaJoseph7 <aleenajoseph18122002@gmail.com>
2025-11-29 17:58:42 +0530

    commit 40 (urls)

--- StudentApp/urls.py
+++ StudentApp/urls.py
@@ -3,4 +3,6 @@ from StudentApp import views
 
 urlpatterns=[
     path('StudentHomePage/',views.StudentHomePage,name="StudentHomePage"),
+    path('StudentLoginPage/',views.StudentLoginPage,name="StudentLoginPage"),
+
 ]
