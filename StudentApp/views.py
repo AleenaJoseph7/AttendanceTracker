2556bb3dcd9f89dc82121de8e39b78805b8e3387
AleenaJoseph7 <aleenajoseph18122002@gmail.com>
2025-11-29 17:57:18 +0530

    commit 40 (alignment)

--- StudentApp/views.py
+++ StudentApp/views.py
@@ -1,20 +1,22 @@
-from django.shortcuts import render,redirect
-from TeacherApp.models import Studentdb,Internalmarkdb,ChatMessage,Attendancedb,Subjectdb
+from django.shortcuts import render, redirect
+from TeacherApp.models import Studentdb, Internalmarkdb, ChatMessage, Attendancedb, Subjectdb
 
 
 # Create your views here.
 def StudentHomePage(request):
-    return render(request,"base.html")
+    return render(request, "base.html")
+
 
 def StudentLoginPage(request):
-    return render(request,"StudentLoginPage.html")
+    return render(request, "StudentLoginPage.html")
+
 
 def StudentLogin(request):
-    if request.method=='POST':
-        regid=request.POST.get('regid')
-        password=request.POST.get('password')
+    if request.method == 'POST':
+        regid = request.POST.get('regid')
+        password = request.POST.get('password')
 
-        if Studentdb.objects.filter(Student_regid=regid,Student_password=password).exists():
+        if Studentdb.objects.filter(Student_regid=regid, Student_password=password).exists():
             return redirect(StudentHomePage)
         else:
             return redirect(StudentLoginPage)
