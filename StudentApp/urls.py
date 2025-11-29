b9244564cf70620872c2c84ed613a942abe05963
AleenaJoseph7 <aleenajoseph18122002@gmail.com>
2025-11-29 17:59:49 +0530

    commit 40 (alignment of urls)

--- StudentApp/urls.py
+++ StudentApp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from StudentApp import views
 
-urlpatterns=[
-    path('StudentHomePage/',views.StudentHomePage,name="StudentHomePage"),
-    path('StudentLoginPage/',views.StudentLoginPage,name="StudentLoginPage"),
-    path('StudentLogin/',views.StudentLogin,name="StudentLogin"),
+urlpatterns = [
+    path('StudentHomePage/', views.StudentHomePage, name="StudentHomePage"),
+    path('StudentLoginPage/', views.StudentLoginPage, name="StudentLoginPage"),
+    path('StudentLogin/', views.StudentLogin, name="StudentLogin"),
 ]
